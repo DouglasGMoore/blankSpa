50ba18fdde6b1b92b57039e62b1904fd2bd9a827
DouglasGMooore <douglasg.moore@gmail.com>
2019-06-24 11:29:46 -0500

    more clean up

--- components/pages/Info.js
+++ components/pages/Info.js
@@ -1,16 +1,7 @@
 export default function(){
     return `
 
-<p >Hello!</p>
-<p class="bio">Thank you for checking out my portfolio! I hope you like it. I am just beginning
-  my journey as a web developer. I already have experience with HTML/CSS, JavaScript, Pyhton, and C#.</p>
-<p class="bio">I especially love JavaScript and Python.
-I have taken classes on HTML/CSS and JavaScript from freecodecamp.org and CodeAcademy.com. I am
-currently participating in a Javascript bootcamp through Savvy Coders.</p>
-<p class="bio">In the future, I hope to specialiize in full-stack web developement and then graduate
-  into AI and deep learning. </p>
-<p class="bio">I am also gaining experience and proficiency on Linux. I have used Centos, Mint, Manjaro, and Ubuntu</p>
-</div>
+
 `
     ;
 }
